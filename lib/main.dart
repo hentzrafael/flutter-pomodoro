44a2a23ef2f44d3600dea7e969208af1fe3a56bc
Rafael Hentz <407264@unoesc.lan>
2022-07-27 15:13:05 -0300

    Removed debug tag

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,8 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Pomodoro',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
@@ -28,41 +29,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter_mobx/flutter_mobx.dart';
-// import 'package:pomodoro/store/contador.store.dart';
-// final store = ContadorStore();
-
-// class HomePage extends StatelessWidget {
-//   const HomePage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Center(child:  Text('Contador')),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Observer(
-//               builder: (_) => Text(
-//                 '${store.contador}',
-//                 style: Theme.of(context).textTheme.headline4,
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: store.incrementar,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
