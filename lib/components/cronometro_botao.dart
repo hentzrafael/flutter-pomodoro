ca83462c316bf1c4d91a028f1a8d55740b353b42
Rafael Hentz <407264@unoesc.lan>
2022-07-27 14:54:28 -0300

    Project done

---
+++ lib/components/cronometro_botao.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CronometroBotao extends StatelessWidget {
+  final String texto;
+  final IconData icone;
+  final void Function()? click;
+
+  const CronometroBotao({
+    Key? key,
+    required this.texto,
+    required this.icone,
+    this.click,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          primary: Colors.black,
+          padding: const EdgeInsets.symmetric(
+            vertical: 20,
+            horizontal: 30,
+          ),
+          textStyle: const TextStyle(
+            fontSize: 20,
+          )),
+      onPressed: click,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 10,
+            ),
+            child: Icon(
+              icone,
+              size: 35,
+            ),
+          ),
+          Text(
+            texto,
+          )
+        ],
+      ),
+    );
+  }
+}
